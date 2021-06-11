a986f5b8868ca9acb2a1be4ddbd3c1aad1c81749
Alpha <mavamalonga.alpha@gmail.com>
2021-06-12 00:05:41 +0200

    function message historical success

--- app/static/js/ajax.js
+++ app/static/js/ajax.js
@@ -1,17 +1,26 @@
 
 
-var messageHistory = function(message){
-  var divChat = document.getElementById('chat');
+var messageHistory = function(user, message){
 
-  var bulle = document.createElement('div').appendChild( document.createElement('p') );
+  if (user=='guest') {
+    var divChat = document.getElementById('chat');
+    var bulle = document.createElement('div').appendChild( document.createElement('p') );
+    var att = document.createAttribute('class');
+    att.value = 'bulle-guest';
+    bulle.setAttributeNode(att);
+    bulle.innerHTML = `${message}`;
+    divChat.insertBefore(bulle, divChat.children[-1]);
 
-  var att = document.createAttribute('class');
-  att.value = 'bulle-guest';
-  bulle.setAttributeNode(att);
+  }else if(user=='grandpy'){
+    var divChat = document.getElementById('chat');
+    var bulle = document.createElement('div').appendChild( document.createElement('p') );
+    var att = document.createAttribute('class');
+    att.value = 'bulle-grandpy';
+    bulle.setAttributeNode(att);
+    bulle.innerHTML = `${message}`;
+    divChat.insertBefore(bulle, divChat.children[-1]);
 
-  bulle.innerHTML = `${message}`;
-
-  divChat.insertBefore(bulle, divChat.children[-1]);
+  }
 }
 
 
@@ -20,11 +29,14 @@ function makeRequest(event) {
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       response = JSON.parse(this.responseText)
-      document.querySelector('.bulle-grandpy p').innerHTML = response['text']
+      messageHistory('grandpy', response['text'])
 
+      /*
+      document.querySelector('.bulle-grandpy p').innerHTML = response['text']
       var grandpy = document.querySelector('.bulle-grandpy');
       var divChat = document.getElementById('chat');
       divChat.insertBefore(grandpy, divChat.children[-1]);
+      */
 
     }
   };
@@ -32,7 +44,7 @@ function makeRequest(event) {
   let endpoint = "http://127.0.0.1:5000/api"
   let url = endpoint.concat('/', message);
 
-  messageHistory(message);
+  messageHistory('guest', message);
 
   xhttp.open("GET", url, true);
   xhttp.send();
